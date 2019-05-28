a9ff6cd04d01d3c2cfbe3491658c32afaf1515c3
Ozymern <alejandro.fvaras@gmail.com>
2019-05-28 14:30:33 -0400

    add comment

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_basic/src/pages/count_page.dart';
 
 class MyApp extends StatelessWidget {
-  // build contiene la informacion del arbol de Widget
   @override
+  // build ()metodo para redibujar un widget
+  //BuildContext La clase no es más que una referencia a la ubicación de un Widget dentro de la estructura de árbol de todos los Widgets que se crea,contiene la informacion del arbol de Widget
   Widget build(BuildContext context) {
     //MaterialApp permite la confguracion global de la aplicacion
     return MaterialApp(
